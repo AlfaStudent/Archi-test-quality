22b0413c9e806f49a7b34e72e3e76396b61160c2
Alpha <abdoul.conde99@gmail.com>
2024-10-04 13:43:37 +0200

    ajouter du repository et de ProductEntity

---
+++ src/order/infrastructure/persistance/product.repository.ts
@@ -0,0 +1,44 @@
+import { InjectDataSource } from '@nestjs/typeorm';
+import { Product } from 'src/order/domain/entity/product.entity';
+import { ProductRepositoryInterface } from 'src/order/domain/port/persistance/product.repository.interface';
+import { DataSource, Repository } from 'typeorm';
+
+export default class ProductRepositoryTypeOrm
+  extends Repository<Product>
+  implements ProductRepositoryInterface
+{
+  constructor(@InjectDataSource() private readonly datasource: DataSource) {
+    super(Product, datasource.createEntityManager());
+  }
+
+  async findById(id: string): Promise<Product | null> { 
+    const queryBuilder = this.createQueryBuilder('product');
+
+    
+    queryBuilder.where('order.id = :id', { id });
+
+    return queryBuilder.getOne();
+  }
+
+  async findAll(): Promise<Product[]> {
+    const queryBuilder = this.createQueryBuilder('product');
+
+    return queryBuilder.getMany();
+  }
+
+  async findByCustomerName(customerName: string): Promise<Product[]> {
+    const queryBuilder = this.createQueryBuilder('product');
+
+    queryBuilder.where('product.customerName = :customerName', { customerName });
+
+    return queryBuilder.getMany();
+  }
+
+  async deleteProduct(id: string): Promise<void> {
+    const queryBuilder = this.createQueryBuilder('product');
+
+    queryBuilder.where('order.id = :id', { id });
+
+    await queryBuilder.delete().execute();
+  }
+}
